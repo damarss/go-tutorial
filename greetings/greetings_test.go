f303fd5b1f8e92c4092d28c6ac75fc8210d37034
Damars <damarsepti627@gmail.com>
2023-06-28 10:26:09 +0700

    Unit test

---
+++ greetings/greetings_test.go
@@ -0,0 +1,26 @@
+package greetings
+
+import (
+	"testing"
+	"regexp"
+)
+
+// Test for valid return value
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	msg, err := Hello("Gladys")
+	
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+// Test for error return value
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
